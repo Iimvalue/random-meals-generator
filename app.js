68339f72d70fa8a7c15b6b92b917f2ae041377b6
Abdulrahman <1xvlux1@gmail.com>
2023-05-18 18:09:49 +0300

    add empty functions with comment explanations

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-$(document).ready(function(){
-    $.get("www.themealdb.com/api/json/v1/1/random.php")
-        .done(function (data){
-        console.log(data);
-    })
+$(document).ready(function () {
+
+    // #get-meal is a button to start the process of getting meal
+    $('#get-meal').click(function (e) {})
+
+    // startToGetMeal function is start call function to fetch meal of food and drinks
+    function startToGetMeal() {}
+
+    // fetchMeal is a function to fetch API url as type of food or drink as array, send array to appendMealIntoDocument function.
+    function fetchMeal(url, type) {}
+
+    // appendMealIntoDocument is a function to display a meal on website as card format
+    function appendMealIntoDocument(data, type) {
+        // data is variable array that receive from fetchMeal function.
+        // type is variable that receive from fetchMeal for deals with data index name.
+    }
+
+    // pinStatus is a function to check the status of Pin button is it pinned or not
+    function pinStatus() {}
+
+    // addIngredientsIntoTooltipButton is a function to add Ingredients into a toolTip button
+    function addIngredientsIntoTooltipButton(data) {
+        // data is variable array that receive from appendMealIntoDocument function for fetch ingredients.
+    }
 });
+
 
